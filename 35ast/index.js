6524365311ec596c347952e71196ae4a9ffeebdd
liguigong <liguigong@joyy.com>
2023-08-08 20:01:30 +0800

    feat: 更新文档

---
+++ 35ast/index.js
@@ -0,0 +1,297 @@
+{
+  'id';
+:
+  'a4a9fba2a', 'type';
+:
+  'Stage', 'props';
+:
+  {
+    'backgroundColor';
+  :
+    '#EAEDF1', 'width';
+  :
+    '600px', 'fullWidth';
+  :
+    'normal-width';
+  }
+,
+  'children';
+:
+  [{
+    'id': '84ba788da',
+    'type': 'Header',
+    'props': {
+      'backgroundColor': '#ffffff',
+      'borderLeft': '1px none #ffffff',
+      'borderRight': '1px none #ffffff',
+      'borderTop': '1px none #ffffff',
+      'borderBottom': '1px none #ffffff',
+      'paddingTop': '0px',
+      'paddingBottom': '0px',
+      'paddingLeft': '0px',
+      'paddingRight': '0px',
+      'cols': [12]
+    },
+    'children': [{
+      'id': '98d909a48',
+      'type': 'Column',
+      'props': {},
+      'children': [{
+        'id': '8298cb99a',
+        'type': 'TextSet',
+        'props': {
+          'list': [{
+            'borderColor': '#ffffff',
+            'paddingBottom': '0px',
+            'borderWidth': '1px',
+            'paddingRight': '10px',
+            'paddingTop': '0px',
+            'borderStyle': 'none',
+            'paddingLeft': '10px',
+            'content': '<p style="line-height: 3; text-align: right;"><span style="color: #000000; font-size: 12px; font-family: arial, sans-serif;"><a style="color: #000000;" href="${viewInWebApiUrl}">Can\'t see the email? Please <span style="color: #3598db;">click here</span></a></span></p>'
+          }],
+          'containerBackgroundColor': 'transparent',
+          'paddingLeft': '10px',
+          'paddingRight': '10px',
+          'paddingTop': '10px',
+          'paddingBottom': '10px'
+        },
+        'children': []
+      }, {
+        'id': '95aaf895a',
+        'type': 'TextSet',
+        'props': {
+          'list': [{
+            'borderColor': '#000',
+            'paddingBottom': '0px',
+            'borderWidth': '1px',
+            'paddingRight': '10px',
+            'paddingTop': '0px',
+            'borderStyle': 'none',
+            'paddingLeft': '10px',
+            'content': '<p style="font-family: arial,helvetica,sans-serif,Arial, Helvetica, sans-serif;" data-mce-style="font-family: arial,helvetica,sans-serif,Arial, Helvetica, sans-serif;">118-2模版内容</p>'
+          }],
+          'containerBackgroundColor': 'transparent',
+          'paddingLeft': '10px',
+          'paddingRight': '10px',
+          'paddingTop': '10px',
+          'paddingBottom': '10px',
+          'paddingCondition': true
+        },
+        'children': []
+      }]
+    }]
+  }, {
+    'id': '84ba7bbda',
+    'type': 'Section',
+    'props': {
+      'backgroundColor': '#ffffff',
+      'borderLeft': '1px none #ffffff',
+      'borderRight': '1px none #ffffff',
+      'borderTop': '1px none #ffffff',
+      'borderBottom': '1px none #ffffff',
+      'paddingTop': '0px',
+      'paddingBottom': '0px',
+      'paddingLeft': '0px',
+      'paddingRight': '0px',
+      'cols': [12]
+    },
+    'children': [{
+      'id': '8cab9aa48',
+      'type': 'Column',
+      'props': {},
+      'children': [{
+        'id': '9baabb838',
+        'type': 'Navigation',
+        'props': {
+          'moduleList': [{
+            'title': 'LINK',
+            'link': '',
+            'linkId': 'ecac47bb-e638-4402-a023-83935656ca42'
+          }, { 'title': 'LINK', 'link': '', 'linkId': '32e77e24-1881-4833-8cb5-436ea9403451' }, {
+            'title': 'LINK',
+            'link': '',
+            'linkId': '597bf442-d478-4188-9676-5dee4ed29d8f'
+          }],
+          'color': '#242833',
+          'fontSize': '14px',
+          'fontFamily': 'arial,helvetica,sans-serif,Arial, Helvetica, sans-serif',
+          'segmentLineColor': '#242833',
+          'segmentLineStyle': 'solid',
+          'segmentLineWeight': '1px',
+          'paddingLeft': '10px',
+          'paddingRight': '10px',
+          'paddingTop': '10px',
+          'paddingBottom': '10px',
+          'paddingCondition': true,
+          'containerBackgroundColor': 'transparent'
+        },
+        'children': []
+      }]
+    }]
+  }, {
+    'id': 'b8bbabad9',
+    'type': 'Footer',
+    'props': {
+      'backgroundColor': '#ffffff',
+      'borderLeft': '1px none #ffffff',
+      'borderRight': '1px none #ffffff',
+      'borderTop': '1px none #ffffff',
+      'borderBottom': '1px none #ffffff',
+      'paddingTop': '0px',
+      'paddingBottom': '0px',
+      'paddingLeft': '0px',
+      'paddingRight': '0px',
+      'cols': [12]
+    },
+    'children': [{
+      'id': 'b3bcabad7', 'type': 'Column', 'props': {}, 'children': [{
+        'id': 'b3a9b8ad8', 'type': 'Discount', 'props': {
+          'discountTermsFontFamily_SHOPIFY': 'arial,helvetica,sans-serif,Arial, Helvetica, sans-serif',
+          'discountTermsFontFamily_EC1': 'arial,helvetica,sans-serif,Arial, Helvetica, sans-serif',
+          'discountCodeSize_SHOPIFY': '20px',
+          'discountCodeSize_EC1': '20px',
+          'discountCodeColor_EC1': '#E02020',
+          'discountCodeColor_SHOPIFY': '#E02020',
+          'discountCodeBackgroundColor': '#FFFFFF',
+          'discountCodeBackgroundColor_SHOPIFY': '#FFFFFF',
+          'discountCodeBackgroundColor_EC1': '#FFFFFF',
+          'discountCodeBorderStyle': 'none',
+          'discountCodeBorderStyle_SHOPIFY': 'none',
+          'discountCodeBorderStyle_EC1': 'none',
+          'discountCodeBorderWidth': '1px',
+          'discountCodeBorderWidth_SHOPIFY': '1px',
+          'discountCodeBorderWidth_EC1': '1px',
+          'discountCodeBorderColor': '#000000',
+          'discountCodeBorderColor_SHOPIFY': '#000000',
+          'discountCodeBorderColor_EC1': '#000000',
+          'discountCodeContent_EC1': null,
+          'effectiveTimeColor_SHOPIFY': '#7A8499',
+          'btnFontFamily_SHOPIFY': 'arial,helvetica,sans-serif,Arial, Helvetica, sans-serif',
+          'btnTextSize_SHOPIFY': '16px',
+          'btnTextColor_SHOPIFY': '#FFFFFF',
+          'btnBacdgroundColor_SHOPIFY': '#000000',
+          'buttonRadius_SHOPIFY': '0px',
+          'buttonBorderStyle_SHOPIFY': 'none',
+          'borderWidth_SHOPIFY': '1px',
+          'borderColor_SHOPIFY': '#000000',
+          'insideBackgroundColor_SHOPIFY': '#FFFFFF',
+          'containerBackgroundColor_EC1': 'transparent',
+          'paddingLeft_EC1': '10px',
+          'paddingRight_EC1': '10px',
+          'paddingTop_EC1': '10px',
+          'paddingBottom_EC1': '10px',
+          'paddingCondition_EC1': true,
+          'containerBackgroundColor_SHOPIFY': 'transparent',
+          'paddingLeft_SHOPIFY': '10px',
+          'paddingRight_SHOPIFY': '10px',
+          'paddingTop_SHOPIFY': '10px',
+          'paddingBottom_SHOPIFY': '10px',
+          'paddingCondition_SHOPIFY': true,
+          'discountType_SHOPIFY': 'percentage',
+          'discountTypePercentageValue_SHOPIFY': null,
+          'discountTypeFixedAmountValue_SHOPIFY': null,
+          'discountType_EC2': 'percentage',
+          'discountTypePercentageValue': null,
+          'discountTypeFixedAmountValue': null,
+          'preferentialConditionsType_SHOPIFY': 0,
+          'preferentialConditionsType': 0,
+          'preferentialConditionsSpecifiedAmount_SHOPIFY': null,
+          'preferentialConditionsSpecifiedQuantity_SHOPIFY': null,
+          'preferentialConditionsSpecifiedAmount': null,
+          'preferentialConditionsSpecifiedQuantity': null,
+          'discountUsageRestrictions_SHOPIFY': [0],
+          'discountUsageRestrictions': [],
+          'discountTotalUsageLimit_SHOPIFY': 34,
+          'discountTotalUsageLimit': null,
+          'discountUserUsageLimit': null,
+          'discountUserUsageLimit_SHOPIFY': 1,
+          'effectiveTimeType_SHOPIFY': 0,
+          'effectiveTimeType': 0,
+          'effectiveTimeDay_SHOPIFY': null,
+          'effectiveTimeDay': null,
+          'effectiveTimeFormat_SHOPIFY': 'mm/dd/yyyy',
+          'effectiveTimeFormat': 'mm/dd/yyyy',
+          'couponCodeType': 0,
+          'couponCodeType_SHOPIFY': 0,
+          'discountCode': '',
+          'discountCodeSource': {
+            'valueType': 'string',
+            'startsAt': '',
+            'code': '',
+            'customerGets': [],
+            'endsAt': '',
+            'title': ''
+          },
+          'insideTitle': '<p style="font-family: arial,helvetica,sans-serif,Arial, Helvetica, sans-serif; font-size: 24px; text-align: center;"><strong>Welcome</strong></p><p \n    style="font-family: arial,helvetica,sans-serif,Arial, Helvetica, sans-serif; font-size: 18px; text-align: center;">Thanks for your joining! To express our gratitude, please receive this coupon as a gift, enjoy your stay here!</p>',
+          'discountCodeColor': '#E02020',
+          'discountCodeSize': '20px',
+          'discountTermsFontFamily': 'arial,helvetica,sans-serif,Arial, Helvetica, sans-serif',
+          'btnTextSize': '16px',
+          'btnTextColor': '#FFFFFF',
+          'btnFontFamily': 'arial,helvetica,sans-serif,Arial, Helvetica, sans-serif',
+          'buttonRadius': '0px',
+          'btnBacdgroundColor': '#000000',
+          'buttonBorderStyle': 'none',
+          'borderWidth': '1px',
+          'borderColor': '#000000',
+          'displayEffectiveTime': true,
+          'effectiveTimeColor': '#7A8499',
+          'insideBackgroundWay': 1,
+          'insideBackgroundColor': '#FFFFFF',
+          'insideBackgroundIcon': '',
+          'outsideDisplay': 'POPUP',
+          'outsideTitle': '<p><span style="font-family: arial,helvetica,sans-serif,Arial, Helvetica, sans-serif; font-size: 18px;">Don\'t forget to use your coupon code</span></p>',
+          'outsideBtnTextContent': 'Copy',
+          'outsideBackgroundBar': 1,
+          'outsideBackgroundPop': 11,
+          'outsideBackgroundColorBar': '#FFCA3D',
+          'outsideBackgroundColorPop': '#FFCA3D',
+          'outsideBackgroundIconPop': '',
+          'outsideBorderStyle': 'none',
+          'outsideBorderRadius': '0px',
+          'outsideBtnBackgroundColor': '#000000',
+          'outsideFontFamily': 'arial,helvetica,sans-serif,Arial, Helvetica, sans-serif',
+          'outsideBtnTextSize': '16px',
+          'outsideBtnTextColor': '#FFFFFF',
+          'outsideBorderWidth': '1px',
+          'outsideBorderColor': '#000000',
+          'discountTab': 0,
+          'paddingLeft': '10px',
+          'paddingRight': '10px',
+          'paddingTop': '10px',
+          'paddingBottom': '10px',
+          'paddingCondition': true,
+          'containerBackgroundColor': 'transparent',
+          'platform': 'EC2',
+          'isShowDiscountButton': true,
+          'discountConditionType': 'discount_condition',
+          'displayEffectiveTimeType': 'mm/dd/yyyy',
+          'btnLinkType': 'custom',
+          'href': '',
+          'btnText': 'Acquire coupon code',
+          'btnText2': 'Use discount now',
+          'btnText_SHOPIFY': 'Use discount now',
+          'btnText_CUSTOM': 'Use discount now',
+          'btnUrl': 'https://nanfeiyan12.myshopify.com',
+          'btnUrl2': '',
+          'btnUrl_SHOPIFY': '',
+          'btnUrl_CUSTOM': ''
+        }, 'children': []
+      }, {
+        'id': 'b39b6a94a',
+        'type': 'Subscribe',
+        'props': { 'content': '', 'containerBackgroundColor': 'transparent' },
+        'children': []
+      }]
+    }]
+  }], 'extend';
+:
+  {
+    'version';
+  :
+    '1.0.0', 'updateTime';
+  :
+    '2023-03-13T09:11:48.462Z';
+  }
+}
