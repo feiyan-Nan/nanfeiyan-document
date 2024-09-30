13959a58c6c5d8a85d4738894edb511b6ad18206
liguigong <liguigong@joyy.com>
2024-09-30 11:03:30 +0800

    race 实现自定义接口超时

--- 05回调(callback)-链式(Promise)-等待(async...await))/Promise.race实现.js
+++ 05回调(callback)-链式(Promise)-等待(async...await))/Promise.race实现.js
@@ -10,3 +10,26 @@ Promise.race1 = function(promises) {
 Promise.race1([new Promise((resolve, reject) => {
   setTimeout(reject,2000, '哈哈哈哈')
 }), 2]).then(res => console.log(res)).catch(err => console.log(err))
+
+
+
+// 封装一个超时的逻辑
+function wrap(userPromise) {
+  let abort;
+  let innerPromise = new Promise((resolve, reject) => {
+    abort = reject;
+  });
+  let racePromise = Promise.race([innerPromise, userPromise]);
+  racePromise.abort = abort;
+  return racePromise
+}
+
+let p = wrap(new Promise((resolve, reject) => {
+  setTimeout(resolve, 2000, '用户数据')
+}));
+
+setTimeout(() => {
+  p.abort('超时了')
+}, 1000);
+
+p.then(res => console.log(res)).catch(err => console.log(err))
